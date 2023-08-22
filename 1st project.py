f7237a0d64e0ad0eddaf1603c6a16f5c2b712a8c
prajjwalsilwal <142755665+prajjwalsilwal@users.noreply.github.com>
2023-08-22 15:24:54 -0500

    Update 1st project.py
    Added comments

--- 1st project.py
+++ 1st project.py
@@ -1,24 +1,33 @@
+#Ask the user for the input value
 user_input = float(input("Enter your value: "))
+#The first loop so that if an error happens it will be useful
 while True:
+    #Defines a function to convert to Celsius
     def conv_cels(x):
         return (x-32) * (5/9)
 
+    #Defines a function to convert to Fahrenheit
     def conv_fahr(x):
         return (x* (9/5))+32
+
+    #Asks the user for their desired conversion. Users have to enter 1 or 2
     selection = int(input("Do you want to convert it to 1.Celsius or 2. Fahrenheit: "))
 
+    #This calls the above conv_cels function and they exit out of the loop
     if selection == 1:
         choice = "Celsius"
         result = round(conv_cels(user_input),2)
         break
 
+    #This calls the above conv_fahr function and they exit out of the loop
     elif selection == 2:
         choice = "Fahrenheit"
         result = round(conv_fahr(user_input),2)
         break
 
+    #If user input is not 1 or 2 this prints the message and runs the loop again
     else:
         print("Invalid input")
         continue
-
+#Final output message
 print("Your value was: ",user_input,". You wanted to convert it to: ",choice,". The result is:",result,sep="")
